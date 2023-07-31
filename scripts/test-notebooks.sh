1970f7d40ff7c66dcb033de70c9eacfe83c296cb
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-07-31 15:33:38 +0300

    Update test-notebooks.sh

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -36,26 +36,26 @@ jupyter nbconvert --to notebook --execute integrations/wandb/2_feature_view_crea
 jupyter nbconvert --to notebook --execute integrations/wandb/3_model_training.ipynb
 
 
-# Electricity Prices
-cd advanced_tutorials/electricity
-jupyter nbconvert --to notebook --execute 1_backfill_feature_groups.ipynb
-jupyter nbconvert --to notebook --execute 2_feature_pipeline.ipynb
-jupyter nbconvert --to notebook --execute 3_feature_views_and_training_dataset.ipynb
-jupyter nbconvert --to notebook --execute 4_model_training.ipynb
-jupyter nbconvert --to notebook --execute 5_batch_predictions.ipynb
+# # Electricity Prices
+# cd advanced_tutorials/electricity
+# jupyter nbconvert --to notebook --execute 1_backfill_feature_groups.ipynb
+# jupyter nbconvert --to notebook --execute 2_feature_pipeline.ipynb
+# jupyter nbconvert --to notebook --execute 3_feature_views_and_training_dataset.ipynb
+# jupyter nbconvert --to notebook --execute 4_model_training.ipynb
+# jupyter nbconvert --to notebook --execute 5_batch_predictions.ipynb
 
-cd ../..
+# cd ../..
 
-# NYC Taxi Trips
-cd advanced_tutorials/nyc_taxi_fares/
+# # NYC Taxi Trips
+# cd advanced_tutorials/nyc_taxi_fares/
 
-jupyter nbconvert --to notebook --execute 1_backfill_feature_groups.ipynb
-jupyter nbconvert --to notebook --execute 2_features_pipeline.ipynb
-jupyter nbconvert --to notebook --execute 3_feature_view_and_dataset_creation.ipynb
-jupyter nbconvert --to notebook --execute 4_model_training_and_registration.ipynb
+# jupyter nbconvert --to notebook --execute 1_backfill_feature_groups.ipynb
+# jupyter nbconvert --to notebook --execute 2_features_pipeline.ipynb
+# jupyter nbconvert --to notebook --execute 3_feature_view_and_dataset_creation.ipynb
+# jupyter nbconvert --to notebook --execute 4_model_training_and_registration.ipynb
 
 
-cd ../..
+# cd ../..
 
 
 
