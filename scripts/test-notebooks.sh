8d85715b5ca8a2c1a309f0ec01f3fba04ba122e2
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-08-02 18:00:17 +0300

    Credit Scores

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -27,14 +27,21 @@ jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 # Advanced Tutorials
 cd advanced_tutorials
 
+# # Citibike
+# jupyter nbconvert --to notebook --execute citibike/1_citibike_feature_backfill.ipynb 
+# jupyter nbconvert --to notebook --execute citibike/2_citibike_feature_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute citibike/3_citibike_training_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ipynb 
+
+# Credit Scores
+jupyter nbconvert --to notebook --execute credit_scores/1_citibike_feature_backfill.ipynb 
+jupyter nbconvert --to notebook --execute credit_scores/2_credit_scores_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute credit_scores/3_credit_scores_training_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute credit_scores/4_credit_scores_batch_inference.ipynb 
+
+
 # # Air Quality 
 # jupyter nbconvert --to notebook --execute air_quality/1_air_quality_feature_backfill.ipynb 
 # jupyter nbconvert --to notebook --execute air_quality/2_air_quality_feature_pipeline.ipynb 
 # jupyter nbconvert --to notebook --execute air_quality/3_air_quality_training_pipeline.ipynb 
 # jupyter nbconvert --to notebook --execute air_quality/4_air_quality_batch_inference.ipynb 
-
-# Citibike
-jupyter nbconvert --to notebook --execute citibike/1_citibike_feature_backfill.ipynb 
-jupyter nbconvert --to notebook --execute citibike/2_citibike_feature_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute citibike/3_citibike_training_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ipynb 
