c9cc9d8e1e61f1e72abc01fdd3ed91ccaa3c87d9
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-08-02 20:23:37 +0300

    Nyc Taxi Fares + Cleanup Fix

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -39,11 +39,17 @@ cd advanced_tutorials
 # jupyter nbconvert --to notebook --execute credit_scores/3_credit_scores_training_pipeline.ipynb 
 # jupyter nbconvert --to notebook --execute credit_scores/4_credit_scores_batch_inference.ipynb 
 
-# Electricity
-jupyter nbconvert --to notebook --execute electricity/1_electricity_feature_backfill.ipynb 
-jupyter nbconvert --to notebook --execute electricity/2_electricity_feature_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute electricity/3_electricity_training_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute electricity/4_electricity_batch_inference.ipynb 
+# # Electricity
+# jupyter nbconvert --to notebook --execute electricity/1_electricity_feature_backfill.ipynb 
+# jupyter nbconvert --to notebook --execute electricity/2_electricity_feature_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute electricity/3_electricity_training_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute electricity/4_electricity_batch_inference.ipynb 
+
+# Nyc Taxi Fares
+jupyter nbconvert --to notebook --execute nyc_taxi_fares/1_nyc_taxi_fares_feature_backfill.ipynb
+jupyter nbconvert --to notebook --execute nyc_taxi_fares/2_nyc_taxi_fares_feature_pipeline.ipynb
+jupyter nbconvert --to notebook --execute nyc_taxi_fares/3_nyc_taxi_fares_training_pipeline.ipynb
+jupyter nbconvert --to notebook --execute nyc_taxi_fares/4_nyc_taxi_fares_batch_inference.ipynb
 
 
 #-----
