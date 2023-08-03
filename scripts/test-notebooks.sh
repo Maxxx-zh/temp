6b5dbe719d75571f7492b387f204924f0d032da8
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-08-03 18:31:42 +0300

    Putting All together

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -5,39 +5,39 @@ set -e
 # Remove any FGs, FVs, Models, Deployments
 jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 
-# # fraud batch
-# jupyter nbconvert --to notebook --execute fraud_batch/1_fraud_batch_feature_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute fraud_batch/2_fraud_batch_training_pipeline.ipynb
-# jupyter nbconvert --to notebook --execute fraud_batch/3_fraud_batch_inference.ipynb
+# fraud batch
+jupyter nbconvert --to notebook --execute fraud_batch/1_fraud_batch_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute fraud_batch/2_fraud_batch_training_pipeline.ipynb
+jupyter nbconvert --to notebook --execute fraud_batch/3_fraud_batch_inference.ipynb
 
-# # fraud online
-# jupyter nbconvert --to notebook --execute fraud_online/1_fraud_online_feature_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute fraud_online/2_fraud_online_training_pipeline.ipynb
-# jupyter nbconvert --to notebook --execute fraud_online/3_fraud_online_inference_pipeline.ipynb
+# fraud online
+jupyter nbconvert --to notebook --execute fraud_online/1_fraud_online_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute fraud_online/2_fraud_online_training_pipeline.ipynb
+jupyter nbconvert --to notebook --execute fraud_online/3_fraud_online_inference_pipeline.ipynb
 
-# # Remove any FGs, FVs, Models, Deployments
-# jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
+# Remove any FGs, FVs, Models, Deployments
+jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 
-# # churn
-# jupyter nbconvert --to notebook --execute churn/1_churn_feature_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute churn/2_churn_training_pipeline.ipynb
-# jupyter nbconvert --to notebook --execute churn/3_churn_batch_inference.ipynb
+# churn
+jupyter nbconvert --to notebook --execute churn/1_churn_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute churn/2_churn_training_pipeline.ipynb
+jupyter nbconvert --to notebook --execute churn/3_churn_batch_inference.ipynb
 
-# # Great Expectations
-# jupyter nbconvert --to notebook --execute integrations/great_expectations/Great_Expectations_Hopsworks_Concepts.ipynb
-# jupyter nbconvert --to notebook --execute integrations/great_expectations/fraud_batch_data_validation.ipynb
+# Great Expectations
+jupyter nbconvert --to notebook --execute integrations/great_expectations/Great_Expectations_Hopsworks_Concepts.ipynb
+jupyter nbconvert --to notebook --execute integrations/great_expectations/fraud_batch_data_validation.ipynb
 
-# # Remove any FGs, FVs, Models, Deployments
-# jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
+# Remove any FGs, FVs, Models, Deployments
+jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 
 # Advanced Tutorials
 cd advanced_tutorials
 
-# # Citibike
-# jupyter nbconvert --to notebook --execute citibike/1_citibike_feature_backfill.ipynb 
-# jupyter nbconvert --to notebook --execute citibike/2_citibike_feature_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute citibike/3_citibike_training_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ipynb 
+# Citibike
+jupyter nbconvert --to notebook --execute citibike/1_citibike_feature_backfill.ipynb 
+jupyter nbconvert --to notebook --execute citibike/2_citibike_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute citibike/3_citibike_training_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ipynb 
 
 # Remove any FGs, FVs, Models, Deployments
 jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
