cfd5449721ca518312e18c1ebf2b5add14c02332
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-08-14 19:51:43 +0300

    TF Tutorials

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -6,6 +6,7 @@ set -e
 jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 
 # Loan Approval
+jupyter nbconvert --to notebook --execute loan_approval/0-loan-approval-eda.ipynb
 jupyter nbconvert --to notebook --execute loan_approval/1-loan-approval-feature-pipeline.ipynb
 jupyter nbconvert --to notebook --execute loan_approval/2-loan-approval-training-pipeline.ipynb
 jupyter nbconvert --to notebook --execute loan_approval/3-loan-approval-batch-inference.ipynb
@@ -73,3 +74,31 @@ jupyter nbconvert --to notebook --execute nyc_taxi_fares/4_nyc_taxi_fares_batch_
 
 # Remove any FGs, FVs, Models, Deployments
 jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
+
+# Move to transformation_functions folder
+cd transformation_functions
+
+# Keras TF
+jupyter nbconvert --to notebook --execute keras/keras_transformation_functions.ipynb
+
+# Remove any FGs, FVs, Models, Deployments
+jupyter nbconvert --to notebook --execute ../../scripts/cleanup-tutorials.ipynb
+
+# PyTorch TF
+jupyter nbconvert --to notebook --execute pytorch/pytorch_transformation_functions.ipynb
+
+# Remove any FGs, FVs, Models, Deployments
+jupyter nbconvert --to notebook --execute ../../scripts/cleanup-tutorials.ipynb
+
+# Sklearn TF
+jupyter nbconvert --to notebook --execute sklearn/sklearn_transformation_functions.ipynb
+
+# Remove any FGs, FVs, Models, Deployments
+jupyter nbconvert --to notebook --execute ../../scripts/cleanup-tutorials.ipynb
+
+# Custom TF
+python custom/transformations.py
+jupyter nbconvert --to notebook --execute custom/custom_transformation_functions.ipynb
+
+# Remove any FGs, FVs, Models, Deployments
+jupyter nbconvert --to notebook --execute ../../scripts/cleanup-tutorials.ipynb
