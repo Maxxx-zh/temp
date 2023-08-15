29913b9d53c69448103ac8a828525b5735deb615
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-08-15 18:16:42 +0300

    Short Test

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -24,20 +24,20 @@ jupyter nbconvert --to notebook --execute fraud_online/1_fraud_online_feature_pi
 jupyter nbconvert --to notebook --execute fraud_online/2_fraud_online_training_pipeline.ipynb
 jupyter nbconvert --to notebook --execute fraud_online/3_fraud_online_inference_pipeline.ipynb
 
-# Remove any FGs, FVs, Models, Deployments
-jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
+# # Remove any FGs, FVs, Models, Deployments
+# jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 
-# churn
-jupyter nbconvert --to notebook --execute churn/1_churn_feature_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute churn/2_churn_training_pipeline.ipynb
-jupyter nbconvert --to notebook --execute churn/3_churn_batch_inference.ipynb
+# # churn
+# jupyter nbconvert --to notebook --execute churn/1_churn_feature_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute churn/2_churn_training_pipeline.ipynb
+# jupyter nbconvert --to notebook --execute churn/3_churn_batch_inference.ipynb
 
-# Great Expectations
-jupyter nbconvert --to notebook --execute integrations/great_expectations/Great_Expectations_Hopsworks_Concepts.ipynb
-jupyter nbconvert --to notebook --execute integrations/great_expectations/fraud_batch_data_validation.ipynb
+# # Great Expectations
+# jupyter nbconvert --to notebook --execute integrations/great_expectations/Great_Expectations_Hopsworks_Concepts.ipynb
+# jupyter nbconvert --to notebook --execute integrations/great_expectations/fraud_batch_data_validation.ipynb
 
-# Remove any FGs, FVs, Models, Deployments
-jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
+# # Remove any FGs, FVs, Models, Deployments
+# jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 
 # Advanced Tutorials
 cd advanced_tutorials
@@ -51,29 +51,29 @@ jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ip
 # Remove any FGs, FVs, Models, Deployments
 jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
 
-# Credit Scores
-jupyter nbconvert --to notebook --execute credit_scores/1_credit_scores_feature_backfill.ipynb 
-jupyter nbconvert --to notebook --execute credit_scores/2_credit_scores_feature_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute credit_scores/3_credit_scores_training_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute credit_scores/4_credit_scores_batch_inference.ipynb 
+# # Credit Scores
+# jupyter nbconvert --to notebook --execute credit_scores/1_credit_scores_feature_backfill.ipynb 
+# jupyter nbconvert --to notebook --execute credit_scores/2_credit_scores_feature_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute credit_scores/3_credit_scores_training_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute credit_scores/4_credit_scores_batch_inference.ipynb 
 
-# Remove any FGs, FVs, Models, Deployments
-jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
+# # Remove any FGs, FVs, Models, Deployments
+# jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
 
-# Electricity
-jupyter nbconvert --to notebook --execute electricity/1_electricity_feature_backfill.ipynb 
-jupyter nbconvert --to notebook --execute electricity/2_electricity_feature_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute electricity/3_electricity_training_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute electricity/4_electricity_batch_inference.ipynb 
+# # Electricity
+# jupyter nbconvert --to notebook --execute electricity/1_electricity_feature_backfill.ipynb 
+# jupyter nbconvert --to notebook --execute electricity/2_electricity_feature_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute electricity/3_electricity_training_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute electricity/4_electricity_batch_inference.ipynb 
 
-# Nyc Taxi Fares
-jupyter nbconvert --to notebook --execute nyc_taxi_fares/1_nyc_taxi_fares_feature_backfill.ipynb
-jupyter nbconvert --to notebook --execute nyc_taxi_fares/2_nyc_taxi_fares_feature_pipeline.ipynb
-jupyter nbconvert --to notebook --execute nyc_taxi_fares/3_nyc_taxi_fares_training_pipeline.ipynb
-jupyter nbconvert --to notebook --execute nyc_taxi_fares/4_nyc_taxi_fares_batch_inference.ipynb
+# # Nyc Taxi Fares
+# jupyter nbconvert --to notebook --execute nyc_taxi_fares/1_nyc_taxi_fares_feature_backfill.ipynb
+# jupyter nbconvert --to notebook --execute nyc_taxi_fares/2_nyc_taxi_fares_feature_pipeline.ipynb
+# jupyter nbconvert --to notebook --execute nyc_taxi_fares/3_nyc_taxi_fares_training_pipeline.ipynb
+# jupyter nbconvert --to notebook --execute nyc_taxi_fares/4_nyc_taxi_fares_batch_inference.ipynb
 
-# Remove any FGs, FVs, Models, Deployments
-jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
+# # Remove any FGs, FVs, Models, Deployments
+# jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
 
 # Go to transformation_functions folder
 cd transformation_functions 
