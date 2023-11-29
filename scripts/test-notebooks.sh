efc7aa3037c93c12e5e33a7b5202e7faad7703bc
Maksym Zhytnikov <63515947+Maxxx-zh@users.noreply.github.com>
2023-11-29 18:07:57 +0200

    Update test-notebooks.sh

--- scripts/test-notebooks.sh
+++ scripts/test-notebooks.sh
@@ -18,10 +18,10 @@ jupyter nbconvert --to notebook --execute fraud_batch/1_fraud_batch_feature_pipe
 jupyter nbconvert --to notebook --execute fraud_batch/2_fraud_batch_training_pipeline.ipynb
 jupyter nbconvert --to notebook --execute fraud_batch/3_fraud_batch_inference.ipynb
 
-# # fraud online
-# jupyter nbconvert --to notebook --execute fraud_online/1_fraud_online_feature_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute fraud_online/2_fraud_online_training_pipeline.ipynb
-# jupyter nbconvert --to notebook --execute fraud_online/3_fraud_online_inference_pipeline.ipynb
+# fraud online
+jupyter nbconvert --to notebook --execute fraud_online/1_fraud_online_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute fraud_online/2_fraud_online_training_pipeline.ipynb
+jupyter nbconvert --to notebook --execute fraud_online/3_fraud_online_inference_pipeline.ipynb
 
 # Remove any FGs, FVs, Models, Deployments
 jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
@@ -41,20 +41,20 @@ jupyter nbconvert --to notebook --execute scripts/cleanup-tutorials.ipynb
 # Advanced Tutorials
 cd advanced_tutorials
 
-# Citibike
-jupyter nbconvert --to notebook --execute citibike/1_citibike_feature_backfill.ipynb 
-jupyter nbconvert --to notebook --execute citibike/2_citibike_feature_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute citibike/3_citibike_training_pipeline.ipynb 
-jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ipynb 
+# # Citibike
+# jupyter nbconvert --to notebook --execute citibike/1_citibike_feature_backfill.ipynb 
+# jupyter nbconvert --to notebook --execute citibike/2_citibike_feature_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute citibike/3_citibike_training_pipeline.ipynb 
+# jupyter nbconvert --to notebook --execute citibike/4_citibike_batch_inference.ipynb 
 
 # # Remove any FGs, FVs, Models, Deployments
 # jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
 
-# # Credit Scores
-# jupyter nbconvert --to notebook --execute credit_scores/1_credit_scores_feature_backfill.ipynb 
-# jupyter nbconvert --to notebook --execute credit_scores/2_credit_scores_feature_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute credit_scores/3_credit_scores_training_pipeline.ipynb 
-# jupyter nbconvert --to notebook --execute credit_scores/4_credit_scores_batch_inference.ipynb 
+# Credit Scores
+jupyter nbconvert --to notebook --execute credit_scores/1_credit_scores_feature_backfill.ipynb 
+jupyter nbconvert --to notebook --execute credit_scores/2_credit_scores_feature_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute credit_scores/3_credit_scores_training_pipeline.ipynb 
+jupyter nbconvert --to notebook --execute credit_scores/4_credit_scores_batch_inference.ipynb 
 
 # Remove any FGs, FVs, Models, Deployments
 jupyter nbconvert --to notebook --execute ../scripts/cleanup-tutorials.ipynb
